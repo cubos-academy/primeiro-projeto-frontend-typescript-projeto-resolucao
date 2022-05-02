3e6ba710dd19ce0ccbcdfb0cd2b94949698991d7
Daniel <danieldeandradelopes@gmail.com>
2022-05-02 15:04:48 -0300

    created sign page

---
+++ base/src/routes.js
@@ -0,0 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
+import SignIn from './pages/SignIn';
+
+function MainRoutes() {
+  return (
+    <Routes>
+      <Route path='/' element={<SignIn />} />
+
+      <Route path='/main' element={<Main />} />
+    </Routes>
+  )
+}
+
+export default MainRoutes;
